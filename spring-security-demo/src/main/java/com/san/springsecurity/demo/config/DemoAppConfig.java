98f9e66f5af5f996ae35fa8c962c673dcdb693a0
SK Saini <sanjeevsaini11235@gmail.com>
2019-12-04 09:33:48 +0530

    L1

---
+++ spring-security-demo/src/main/java/com/san/springsecurity/demo/config/DemoAppConfig.java
@@ -0,0 +1,12 @@
+package com.san.springsecurity.demo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.san.springsecurity.demo")
+public class DemoAppConfig {
+
+}
